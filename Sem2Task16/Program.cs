6b2ad1587603da386d854a4c0c561c7a018f7e67
Alex Slepov <alekseyslepov2003@gmail.com>
2023-11-24 21:00:08 +0300

    Classwork

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,32 @@
+﻿string? numberOneLine = Console.ReadLine();
+string? numberTwoLine = Console.ReadLine();
+
+int numberOne = int.Parse(numberOneLine);
+int numberTwo = int.Parse(numberTwoLine);
+int x = 0;
+
+if (numberOne > numberTwo)
+{
+    if (numberOne == numberTwo * numberTwo)
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine("no");
+    }
+}
+else
+{
+    x = numberOne;
+    numberOne = numberTwo;
+    numberTwo = x;
+    if (numberOne == numberTwo * numberTwo)
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine("no");
+    }
+}
