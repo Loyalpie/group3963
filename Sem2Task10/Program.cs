1d198e00fcb4d314daf47f22fc568b64da893a9c
Alex Slepov <alekseyslepov2003@gmail.com>
2023-11-28 00:08:45 +0300

    HomeworkSem2

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿string? numberLine = Console.ReadLine();
+
+int number = int.Parse(numberLine);
+int secondDigit = (((number % 100) - (number % 10)) / 10);
+
+Console.WriteLine(secondDigit);
---
+++ Sem2Task15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+string? numberLine = Console.ReadLine();
+int num = int.Parse(numberLine);
+
+if (num > 0 && num < 8)
+{
+    if (num < 6)
+    {
+        Console.WriteLine("Это будний день");
+    }
+    else
+    {
+        Console.WriteLine("Это выходной");
+    }
+}
+else
+{
+    Console.WriteLine("Неверный номер дня ненели...");
+}
