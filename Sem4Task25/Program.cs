3ceeb34b752f97dffaa41f03a7ced0e852240c02
Alex Slepov <alekseyslepov2003@gmail.com>
2023-11-28 22:00:26 +0300

    Sem4Homework

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,75 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//ввод данных
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+double Calculator(int numA, int numB)
+{
+    double result = Math.Pow(numA, numB);
+    return result;
+}
+
+//вывод данных
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+int numberA = ReadData("Введите число A: "); //переменная А
+int numberB = ReadData("Введите число B: "); //переменная B
+
+double result = Calculator(numberA, numberB); //возведение А в степень В
+PrintData($"A в степени B равно: {result.ToString()}"); //вывод результата
+
+//===================================вторая часть программы с калькулятором============================================
+PrintData("============Калькулятор============");
+
+string ReadOperation(string msg)
+{
+    Console.Write(msg);
+    return Console.ReadLine();//int.Parse(Console.ReadLine());
+}
+
+//метод операций (считывает в качестве строки символ (+ - * / ^) и выполняет аналогичную операцию)
+double Operations(string type, int numA, int numB)
+{
+    result = 0; //переменная-результат
+    //сложение
+    if (type == "+")
+    {
+        result = numA + numB;
+    }
+    //вычитание
+    if (type == "-")
+    {
+        result = numA - numB;
+    }
+    //умножение
+    if (type == "*")
+    {
+        result = numA * numB;
+    }
+    //деление
+    if (type == "/")
+    {
+        result = numA / numB;
+    }
+    //возведение в степень
+    if (type == "^")
+    {
+        result = Math.Pow(numA, numB);
+    }
+    return result;
+}
+
+numberA = ReadData("Введите число A: "); //переменная А
+numberB = ReadData("Введите число B: "); //переменная B
+
+string operation = ReadOperation("Введите операцию, которую необходимо сделать с числами А и В (+, -, /, *, ^): ");
+
+result = Operations(operation, numberA, numberB); //расчет результата с помоьщью метода
+PrintData($"Результат операции {numberA} {operation} {numberB} равен {result}"); //вывод результата
---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,75 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//метод ввода данных
+double ReadData(string msg)
+{
+    Console.Write(msg);
+    return double.Parse(Console.ReadLine());
+}
+
+//метод вывода данных
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+double number = ReadData("Введите число N: ");
+string numberLine = number.ToString();
+int len = numberLine.Length;
+
+int[] array = new int[len];
+
+//суммирует элементы массива, в которых хранятся значения разрядов числа
+int Result(double num)
+{
+    double newNum = 0;
+    double numRem = 0;
+    int count = 0;
+    int i = len;
+    while (count < len)
+    {
+        newNum = num / Math.Pow(10, (i - 1));
+        array[count] = (int)newNum;
+        numRem = num % Math.Pow(10, (i - 1));
+        num = numRem;
+        i -= 1;
+        count += 1;
+    }
+
+    int index = 0;
+    int result = 0; //сумма изначально равна 0
+    for (int j = 0; j < len; j++)
+    {
+        result += array[index]; //прибавляем значение array[index]
+        index += 1;
+    }
+    return result;
+}
+
+//находит сумму цифр, но только через строки
+int ResultString(string numLine)
+{
+    int index = 0;
+    int x = 0;
+    int result = 0;
+    string line = String.Empty; //пустая строка, в которую потом будут записываться цифры
+
+    for (int i = 0; i < len; i++)
+    {
+        line = (numLine[i]).ToString();
+        x = int.Parse(line);
+        result += x;
+        //index += 1;
+    }
+    return result;
+}
+
+
+DateTime time1 = DateTime.Now;
+int result = Result(number);
+
+DateTime time2 = DateTime.Now;
+int result2 = ResultString(numberLine);
+
+PrintData($"Сумма цифр числа {number} равна {result.ToString()} по числовому методу. Время {DateTime.Now - time1}");
+PrintData($"Сумма цифр числа {number} равна {result2.ToString()} по строчному методу. Время {DateTime.Now - time2}");
