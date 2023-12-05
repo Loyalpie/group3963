023c318bb72b5183169b03f1aea00a7acf0af613
Alex Slepov <alekseyslepov2003@gmail.com>
2023-12-06 00:30:11 +0300

    Sem6Homework

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+//ввод данных
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+//метод учета введенных чисел (они помещаются в массив)
+int[] EnterNumbers(int quantity)
+{
+
+    int[] array = new int[quantity];
+    for (int i = 0; i < array.Length; i++)
+    {
+        int number = ReadData("Введите число: ");
+        array[i] = number;
+    }
+    return array;
+}
+
+//метод "рисования" одномерного массива
+void Print1DArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine(array[array.Length - 1] + "]");
+}
+
+//метод подсчета чисел больше нуля
+int CountEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++) if (array[i] > 0) count++;
+    return count;
+}
+
+
+int quantity = ReadData("Количество чисел: ");
+int[] array = EnterNumbers(quantity);
+Print1DArray(array);
+Console.WriteLine($"Чисел больше нуля: {CountEvenNumbers(array)}");
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//ввод данных
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+//метод нахождения координат точек пересечения прямых
+string CoordPoints(double b1, double k1, double b2, double k2)
+{
+    double xCoord = (b2 - b1) / (k1 - k2);
+    double yCoord = k1 * xCoord + b1;
+    string answer = "(" + xCoord + " " + yCoord + ")";
+    return answer;
+}
+
+double b1 = ReadData("Введите b1: ");
+double k1 = ReadData("Введите k1: ");
+double b2 = ReadData("Введите b2: ");
+double k2 = ReadData("Введите k2: ");
+
+Console.WriteLine(CoordPoints(b1, k1, b2, k2));
